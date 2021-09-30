bbeca03ce12d2a8917eb390313ab9d430258fbc4
chyyeeah <wilsoncheah5@gmail.com>
2021-09-30 13:26:49 -0700

    implemented geetServerSideProps and getStaticProps

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -3,17 +3,13 @@
 import { jsx } from 'theme-ui'
 import Link from 'next/link';
 
-const Notes = () => {
-  const dummyNotes = new Array(10).fill(0).map((e, i) => {
-    return { id: i, title: i };
-  });
-
+const Notes = ({notes}) => {
   return (
     <div sx={{ variant: 'containers.page' }}>
       <h1>My Notes</h1>
 
       <div sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap' }}>
-        {dummyNotes.map(note => (
+        {notes.map(note => (
           <div key={note.id} sx={{ width: '33%', p: 2 }}>
             <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
               <a sx={{ textDecoration: 'none', cursor: 'pointer' }}>
@@ -27,6 +23,14 @@ const Notes = () => {
       </div>
     </div>
   );
+};
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:3000/api/note/`)
+  const {data} = await res.json()
+  return {
+    props: {notes: data}
+  }
 };
 
 export default Notes;
